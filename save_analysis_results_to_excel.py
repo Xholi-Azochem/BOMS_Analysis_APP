60351fa84f3114477563aec75a57b4a48edc0219
Xholi-Azochem <xholisile@azochem.co.za>
2024-11-25 16:34:23 +0200

    Create save_analysis_results_to_excel.py

---
+++ save_analysis_results_to_excel.py
@@ -0,0 +1,7 @@
+def save_analysis_results_to_excel(analysis_results, buffer):
+    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
+        analysis_results["product_metrics"].to_excel(writer, sheet_name="Product_Metrics")
+        analysis_results["product_complexity"].to_excel(writer, sheet_name="Product_Complexity")
+        analysis_results["component_usage"].to_excel(writer, sheet_name="Component_Usage")
+        cost_dist = pd.DataFrame.from_dict(analysis_results["cost_distribution"], orient="index", columns=["Value"])
+        cost_dist.to_excel(writer, sheet_name="Cost_Distribution")
